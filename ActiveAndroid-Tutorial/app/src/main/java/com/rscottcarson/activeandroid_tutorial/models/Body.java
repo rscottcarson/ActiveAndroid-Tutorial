60abfe6da7a66abbc4cf365beb6b3d83fb16129b
rscottcarson <scott.carson@rscottcarson.com>
2016-09-15 09:53:27 -0600

    Added more model classes

---
+++ ActiveAndroid-Tutorial/app/src/main/java/com/rscottcarson/activeandroid_tutorial/models/Body.java
@@ -0,0 +1,7 @@
+package com.rscottcarson.activeandroid_tutorial.models;
+
+/**
+ * Created by scottcarson on 9/14/16.
+ */
+public class Body extends CarPart {
+}
--- ActiveAndroid-Tutorial/app/src/main/java/com/rscottcarson/activeandroid_tutorial/models/CarPart.java
+++ ActiveAndroid-Tutorial/app/src/main/java/com/rscottcarson/activeandroid_tutorial/models/CarPart.java
@@ -1,11 +1,17 @@
 package com.rscottcarson.activeandroid_tutorial.models;
 
+import android.provider.BaseColumns;
+
 import com.activeandroid.Model;
+import com.activeandroid.annotation.Table;
 
 /**
  * Created by scottcarson on 9/13/16.
  */
+@Table(name = CarPart.CAR_PART_TABLE_NAME, id = BaseColumns._ID)
 public class CarPart extends Model {
+
+    public static final String CAR_PART_TABLE_NAME = "CarPart";
 
     public CarPart(){
         super();
